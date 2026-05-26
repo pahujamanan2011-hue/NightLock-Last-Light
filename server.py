231702a48e39aa5ccaad0ba48893b0fb4497f72e
pahujamanan2011-hue <pahujamanan2011@gmail.com>
2026-05-26 16:55:22 +0530

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-PORT = 8000
-
-server = HTTPServer(("localhost", PORT), SimpleHTTPRequestHandler)
-
-print(f"Running on http://localhost:{PORT}")
-
-server.serve_forever()
